4cc73df7793329eb037763aec7d4403c3f49a53a
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-06-03 15:50:19 -0300

    refactor: model mapper access private fields

--- src/main/java/com/dev/igor/serviceproduto/http/data/response/ProdutoResponseDto.java
+++ src/main/java/com/dev/igor/serviceproduto/http/data/response/ProdutoResponseDto.java
@@ -17,13 +17,7 @@ public class ProdutoResponseDto {
     public Long getId() {
         return id;
     }
-    public void setId(Long id) {
-        this.id = id;
-    }
     public String getDescricao() {
         return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
     }
 }
