e489f38e1f02268b15ea419de8627244fdd7c296
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-06-03 15:22:14 -0300

    refactor: return 201 when creating a produto

--- src/main/java/com/dev/igor/serviceproduto/http/ProdutoController.java
+++ src/main/java/com/dev/igor/serviceproduto/http/ProdutoController.java
@@ -4,10 +4,8 @@ import com.dev.igor.serviceproduto.http.data.request.ProdutoPersistDto;
 import com.dev.igor.serviceproduto.http.data.response.ProdutoResponseDto;
 import com.dev.igor.serviceproduto.model.Produto;
 import com.dev.igor.serviceproduto.service.ProdutoService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/produto")
@@ -20,6 +18,7 @@ public class ProdutoController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ProdutoResponseDto inserir(@RequestBody ProdutoPersistDto dto) {
         Produto produto = new Produto(dto.getDescricao(), dto.getValor());
         Produto produtoPersistido =  produtoService.inserir(produto);
