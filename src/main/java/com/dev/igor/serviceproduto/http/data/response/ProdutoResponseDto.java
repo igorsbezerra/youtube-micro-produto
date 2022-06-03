6f6639fa6f4bd689eb535b7808ef2dc905479f5c
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-06-03 15:34:37 -0300

    refactor: needed for modelmapper to work

--- src/main/java/com/dev/igor/serviceproduto/http/data/response/ProdutoResponseDto.java
+++ src/main/java/com/dev/igor/serviceproduto/http/data/response/ProdutoResponseDto.java
@@ -2,8 +2,12 @@ package com.dev.igor.serviceproduto.http.data.response;
 
 public class ProdutoResponseDto {
 
-    private final Long id;
-    private final String descricao;
+    private Long id;
+    private String descricao;
+
+
+    public ProdutoResponseDto() {
+    }
 
     public ProdutoResponseDto(Long id, String descricao) {
         this.id = id;
@@ -13,7 +17,13 @@ public class ProdutoResponseDto {
     public Long getId() {
         return id;
     }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public String getDescricao() {
         return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 }
