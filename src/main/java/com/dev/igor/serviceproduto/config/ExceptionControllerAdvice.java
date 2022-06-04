384c756bf36ef6a6c8f76feba56c5fed0944216b
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-06-04 09:36:18 -0300

    feat: hide auto-generated return

--- src/main/java/com/dev/igor/serviceproduto/config/ExceptionControllerAdvice.java
+++ src/main/java/com/dev/igor/serviceproduto/config/ExceptionControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.dev.igor.serviceproduto.config;
 
 import com.dev.igor.serviceproduto.http.data.response.Error;
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
+    @Hidden
     @ResponseBody
     @ExceptionHandler(NoResultException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
