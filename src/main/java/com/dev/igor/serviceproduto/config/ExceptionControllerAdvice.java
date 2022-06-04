d730f29d0267ff6fd1ee672be4681c26314c244b
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2022-06-04 10:09:51 -0300

    feat: add url documentation

--- src/main/java/com/dev/igor/serviceproduto/config/ExceptionControllerAdvice.java
+++ src/main/java/com/dev/igor/serviceproduto/config/ExceptionControllerAdvice.java
@@ -2,6 +2,7 @@ package com.dev.igor.serviceproduto.config;
 
 import com.dev.igor.serviceproduto.http.data.response.Error;
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,19 +17,22 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class ExceptionControllerAdvice {
 
+    @Value("${springdoc.swagger-ui-path}")
+    private String urlDocumentation;
+
     @Hidden
     @ResponseBody
     @ExceptionHandler(NoResultException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Error noResult(NoResultException ex) {
-        return new Error("X_100", ex.getMessage());
+        return new Error("X_100", ex.getMessage(), urlDocumentation);
     }
 
     @ResponseBody
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Error methodArgument(MethodArgumentTypeMismatchException ex) {
-        return new Error("X_200", "Parâmetro inválido");
+        return new Error("X_200", "Parâmetro inválido", urlDocumentation);
     }
 
     @ResponseBody
@@ -39,13 +43,13 @@ public class ExceptionControllerAdvice {
                 .getFieldErrors()
                 .stream().map(fieldError -> fieldError.getField() + " " + fieldError.getDefaultMessage())
                 .collect(Collectors.joining());
-        return new Error("X_200", mensagens);
+        return new Error("X_200", mensagens, urlDocumentation);
     }
 
     @ResponseBody
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public Error other(Exception ex) {
-        return new Error("X_300", ex.getMessage());
+        return new Error("X_300", ex.getMessage(), urlDocumentation);
     }
 }
